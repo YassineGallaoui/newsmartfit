4975fa16c08f7b24a92fc9866a4aa26937785b0f
Yassine <22yassine05@gmail.com>
2020-10-10 02:06:48 +0200

    first commit

--- backend/server.js
+++ backend/server.js
@@ -35,8 +35,8 @@ app.use('/rules', rulesRouter);
 app.get('*', function (request, response){
     response.sendFile(path.resolve(buildPath, 'index.html'))
     console.log("sono entrato dentro la funzione speciale!!")
-    console.log("richiesta: "+request)
-    console.log("risposta: "+response)
+    console.log("richiesta: "+JSON.stringify(request))
+    console.log("risposta: "+JSON.stringify(response))
   })
 if(process.env.NODE_ENV === "production"){
     app.use(express.static('build'))
